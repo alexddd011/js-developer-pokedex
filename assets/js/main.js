9b4622c40fae50f1419f1aa43a7c7caa8c297071
alexddd011 <devhome915@gmail.com>
2024-01-19 23:38:42 -0300

    desafio dio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modal = document.querySelector("#modal")
+const modalHeader = document.querySelector('#modal-header') 
+const modalContent = document.querySelector('#modal-content')
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+let backgroundModal = ""
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}"  onClick="showDetails(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -45,3 +49,62 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function showDetails(detailsPokemon){
+    
+    modal.className = "showModal"
+    detailPokemon(detailsPokemon)
+    
+}
+function hideDetails(){
+    modal.classList.remove("showModal")
+    modalHeader.classList.remove(backgroundModal)
+    modalHeader.innerHTML = ""
+    modalContent.innerHTML = ""
+}
+async function detailPokemon(id){
+    let req = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    let res = await req.json()
+    construirModalDetails(res)
+}
+
+function construirModalDetails(detalhe){
+    backgroundModal = detalhe.types[0].type.name
+    img = [detalhe.sprites.front_default, detalhe.sprites.back_default]
+    modalHeader.className = backgroundModal
+    modalHeader.innerHTML = `
+        <h3>${detalhe.name}</h3>
+
+        <div class="modalHeader">
+            <ul>
+                <li><strong>${backgroundModal}</strong></li>
+                <li><strong>${detalhe.types[1].type.name}</strong></li>
+            </ul>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${detalhe.id}.svg">
+        </div>
+    `
+    modalContent.innerHTML = `
+        <table>
+            <tbody>
+                <tr>
+                    <td>abilities</td>
+                    <td>
+                        <ul>
+                            <li><span>${detalhe.abilities[0].ability.name}</span></li>
+                            <li><span>${detalhe.abilities[1].ability.name}</span></li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>weight</td>
+                    <td><span>${detalhe.weight}</span></td>
+                </tr>
+                <tr>
+                    <td>height</td>
+                    <td><span>${detalhe.height}</span></td>
+                </tr>
+            </tbody>
+        </table>
+    `
+    console.log(detalhe)
+}
